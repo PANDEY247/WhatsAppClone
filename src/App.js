7cba9194330663926d01ccc6eb9e86103c34e7a9
sourabh <sp8046904@gmail.com>
2022-12-24 17:08:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return (
+    // Bem naming convention 
+    <div className="app">
+      <div className ="app_body">
+      <Sidebar/>
+      {/* Chat */}
+
+      </div>
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6FAZrt1sWcygXmlbMg1M1MhlFHps5Atw",
+  authDomain: "whats-app-project-d3d04.firebaseapp.com",
+  projectId: "whats-app-project-d3d04",
+  storageBucket: "whats-app-project-d3d04.appspot.com",
+  messagingSenderId: "263306910590",
+  appId: "1:263306910590:web:95601de04b48a255daf12f",
+  measurementId: "G-M52HHL7C8E"
+};
