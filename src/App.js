9d653e1c205f3374fb46e0d38ad895d1efc1010e
sourabh <sp8046904@gmail.com>
2022-12-26 09:55:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import './App.css';
 import {Sidebar} from "./Components/Sidebar";
-import Chat from "./Chat";
+import {Chat} from "./Components/Chat";
 function App() {
   return (
     <div className="app">
--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,18 +1,42 @@
-import { Avatar } from '@mui/material';
 import React from 'react';
+import { Avatar,  IconButton } from "@mui/material"
+// import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import AttachFile from '@mui/icons-material/AttachFile';
+import MoreVert from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
 import "./Chat.css";
-function Chat(){
+export function Chat(){
     return (
         <div className='chat'>
-        <div className='chat_Header'>
-        < Avatar/>
-        </div>
+        <div className='chat_header'>
+    <Avatar src={`https://avatars.dicebear.com/api/human/$seed.svg`}/>
 
-        <div className='chat_Footer'>
+    <div className="chat_headerInfo">
+        <h3>Room name</h3>
+        <p>Last seen at.....</p>
+    </div>
+   
+   <div className='chat_headerRight'>
+    <IconButton>
+        <SearchIcon/>
+    </IconButton>
+    <IconButton>
+        <AttachFile/>
+    </IconButton>
+    <IconButton>
+        <MoreVert/>
+    </IconButton>
+   </div>
+        </div>
+        <div className="chat_body"></div>
+            <div className="chat footer">
+
+            </div>
+        </div>
             
-        </div>
-        </div>
-    )
+        
+        
+    );
 
-}
-export default Chat;
+};
